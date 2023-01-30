aea083eb355d330ca66baf01daa2df58cdd5cb68
Melissa Perez <melmperz@gmail.com>
2023-01-29 19:45:39 -0500

    Add current app.js for my handlebars pages

---
+++ app.js
@@ -0,0 +1,50 @@
+/*************************************
+    SETUP
+**************************************/
+// Setting up express
+const PORT = 3999
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+
+// Setting up handlebars
+const { engine } = require('express-handlebars')
+app.engine(
+  '.hbs',
+  engine({
+    defaultLayout: 'index.hbs',
+    extname: '.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+  })
+)
+app.set('view engine', '.hbs')
+
+
+/*************************************
+  LISTENER
+**************************************/
+app.listen(process.env.PORT || PORT, function () {
+    console.log(
+      'Express started on http://localhost:' +
+        PORT +
+        '; press Ctrl-C to terminate.'
+    )
+  })
+
+
+/*************************************
+  HOME ROUTE
+**************************************/
+// Page to render for home
+app.get('/', function (req, res) {
+    res.render('home.hbs', {
+      layout: 'index.hbs',
+      pageTitle: 'Travel Planner',
+    })
+  })
+  
