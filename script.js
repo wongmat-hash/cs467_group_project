976b6bbff6b64153cef85dd9e2c063961934b038
Matthew Wong <wongmat@oregonstate.edu>
2023-01-31 19:37:43 -0800

    login page and css file are complete JS should be complete just need to tie into the DB now

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    //validate input and check with database
-    //if match load the home page
-    //if it doesnt show error prompt in login/ registration page
+
+    // Validate the input
+    if (!username) {
+        alert("Please enter a username");
+        return;
+    }
+    if (!password) {
+        alert("Please enter a password");
+        return;
+    }
+
+    // Check with the database
+    // ...
 }
 
+
 function register() {
-    var fullName = document.getElementByID("fullName").value;
+    var fullName = document.getElementById("fullName").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
+
     // Validate the input
-    // check if password and confirmPassword match
-    // check if email is already used or not
+    if (!fullName) {
+        alert("Please enter your full name");
+        return;
+    }
+    if (!email) {
+        alert("Please enter your email");
+        return;
+    }
+    if (!password) {
+        alert("Please enter a password");
+        return;
+    }
+    if (!confirmPassword) {
+        alert("Please confirm your password");
+        return;
+    }
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    // Check if email is already used
+    // ...
+
     // If everything is valid, then send the data to the server to save it
-    // If it's not valid show an error message
+    // ...
 }
