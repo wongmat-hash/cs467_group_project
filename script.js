d43b0e0ed68fc4ab5e11e50be32c7ad3beb301b3
Matthew Wong <wongmat@oregonstate.edu>
2023-01-31 22:13:15 -0800

    js template needs to be fixed

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
-function login() {
+function login()
+{
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   var errorMessage = "";
 
-  if (!username) {
+  //error handling to check if the user has a valid account or enters info
+  if(!username)
+  {
     errorMessage = "Please enter a valid username.";
-  } else if (!password) {
+  }
+  else if(!password)
+  {
     errorMessage = "Please enter a valid password.";
-  } else if (username !== "user" || password !== "password") {
+  }
+  else if(username !== "user"||password !== "password")
+  {
     errorMessage = "Incorrect username or password.";
   }
 
   if (errorMessage) {
     document.getElementById("error").innerHTML = errorMessage;
-  } else {
+  }
+  else
+  {
     // logic to log in user
   }
 }
 
 
-function register() {
+function register()
+{
     var fullName = document.getElementById("fullName").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
 
     // Validate the input
-    if (!fullName) {
+    if(!fullName)
+    {
         alert("Please enter your full name");
         return;
     }
-    if (!email) {
+    if(!email)
+    {
         alert("Please enter your email");
         return;
     }
-    if (!password) {
+    if(!password)
+    {
         alert("Please enter a password");
         return;
     }
-    if (!confirmPassword) {
+    if(!confirmPassword)
+    {
         alert("Please confirm your password");
         return;
     }
-    if (password !== confirmPassword) {
+    if(password !== confirmPassword)
+    {
         alert("Passwords do not match");
         return;
     }
@@ -52,4 +67,22 @@ function register() {
 
     // If everything is valid, then send the data to the server to save it
     // ...
+}
+
+function reset()
+{
+  const email = document.getElementById("email").value;
+
+  if (email)
+  {
+    // send a request to the server to reset the password
+    // ...
+    console.log("doing something");
+    // show the confirmation message
+    alert("A password reset link has been sent to your email. Please check your inbox.");
+  }
+  else
+  {
+    alert("Please enter your email.");
+  }
 }
