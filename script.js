9cfa0ad449298b7e6a3b1abdac74ea62b9ecc29f
Matthew Wong <wongmat@oregonstate.edu>
2023-02-02 19:40:32 -0800

    worked on registration function should now use db.query method to pass info to database

--- script.js
+++ script.js
@@ -30,53 +30,64 @@ function login()
 
 function register()
 {
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var confirmPassword = document.getElementById("password-confirm").value;
+  let username = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("password-confirm").value;
 
-  if (username == "")
+  //error handling to see if fields are all completed
+  if (username ==  "" || email == "" || password == "" || confirmPassword == "")
   {
-
-  }
-  if (username ==  "" || email == "" || password == "" || password-confirm == "")
-  {
+    //NEED TO WORK ON GETTING THESE FIELDS TO DISPLAY ERROR MESSAGES ON THE HTML
     alert("all fields must be complete");
     return false;
   }
-    // Validate the input
+  // Validate the USERNAME input (NEED TO BE ABLE TO CHECK AGAINST DB)
   if (!username)
   {
     errorMessage = "Please enter a valid username.";
   }
+  // Validate the EMAIL input (NEED TO BE ABLE TO CHECK AGAINST THE DB)
   else if (!email)
   {
     errorMessage = "Please enter your email.";
   }
+  // Validate the PASSWORD input (NEED TO BE ABLE TO CHECK AGAINST THE DB)
   else if (!password)
   {
     errorMessage = "Please enter a password.";
   }
+  // Validate the 2nd PASSWORD input (NEED TO CHECK AGAINST DB)
   else if (!confirmPassword)
   {
     errorMessage = "Please confirm your password.";
   }
+  // Validate the PASSWORD matches CONFIRM
   else if (password !== confirmPassword)
   {
     errorMessage = "Passwords do not match.";
   }
 
-    // Check if email is already used
-    // ...
-    if (errorMessage)
-    {
-    document.getElementById("error").innerHTML = errorMessage;
+  // Create the INSERT query
+  let insertQuery = "INSERT INTO users (username, email, phone, password) VALUES (?,?,?)";
+
+  // Create an array to store the values for the query including phone which has not been used
+  let insertData = [username, email, phone, password];
+
+  // Execute the query using the db.pool.query method
+  db.pool.query(insertQuery, insertData, function(error, rows, fields) {
+    if (error) {
+      res.write(JSON.stringify(error));
+      res.end();
     }
     else
     {
-    // If everything is valid, then send the data to the server to save it
-    // ...
-  }
+      // Redirect to the homepage or display a success message
+      console.log("User registered successfully!");
+      res.redirect('/login.html') //INSERT URL FOR HOME PAGE WHEN LINKING
+    }
+  });
 }
 
 function reset()
