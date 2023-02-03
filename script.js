214a3fb041b8977e0c2b323f793142105ae9a1b4
Matthew Wong <wongmat@oregonstate.edu>
2023-02-02 20:24:50 -0800

    worked on a ton of JS spaghetti code

--- script.js
+++ script.js
@@ -76,10 +76,12 @@ function register()
   let insertData = [username, email, phone, password];
 
   // Execute the query using the db.pool.query method
-  db.pool.query(insertQuery, insertData, function(error, rows, fields) {
-    if (error) {
+  db.pool.query(insertQuery, insertData, function(error, rows, fields)
+  {
+    if (error)
+    {
       res.write(JSON.stringify(error));
-      res.end();
+      return;
     }
     else
     {
@@ -90,22 +92,126 @@ function register()
   });
 }
 
+//this function will reset the password from the user designated form
+function newpassword()
+{
+  //stores the value of the new password
+  let passwordreset = document.getElementByID("newpassword")
+  if (!newpassword)
+  {
+   alert("password field is empty");
+   return;
+  }
+  //check against the DB for the email to reset
+  let updatePasswordQuery = "UPDATE Users SET password = ? WHERE email = ?";
+  let updatePasswordData = [newPassword, email];                                //NEED TO FIGURE OUT HOW TO GET THE EMAIL HERE 
+  db.pool.query(updatePasswordQuery, updatePasswordData, function (error, result)
+  {
+    if (error)
+    {
+      res.write(JSON.stringify(error));
+      return;
+    }
+    else
+    {
+      alert("Password reset successful!");
+    }
+  }
+};
+
 function reset()
 {
-    console.log("in reset function now!")
+    //grabs from the page by email value
     let email = document.getElementById("email").value;
+    //error handling for empty email field
+    if (!email)
+    {
+      alert("email field is empty")
+      return;
+    }
+
+    //checks against DB for email to reset
     let checkQuery = "SELECT * FROM Users WHERE email = ?";
     let checkData = [email];
     db.pool.query(checkQuery, checkData, function(error, rows, fields) {
         if(error)
         {
-            console.error(error);
-        } else if(rows.length === 0) \
+          res.write(JSON.stringify(error));
+          return;
+        }
+        else if(rows.length === 0)
         {
             alert("Email not found.");
-        } else
+        }
+        else
         {
-            res.rediret('/resetpw.html')
+            //found email and we can begin reset
+            let newPassword = generatePassword();
+            let updatePasswordQuery = "UPDATE Users SET password = ? WHERE email = ?";
+            let updatePasswordData = [newPassword, email];
+            db.pool.query(updatePasswordQuery, updatePasswordData, function(error, rows, fields)
+            {
+              if (error)
+              {
+                res.write(JSON.stringify(error));
+                return;
+              }
+              sendPasswordResetEmail(email, newPassword)
+            });
         }
     });
+}
+
+// this function will generate a new password and pass it back
+function generatePassword(length)
+{
+    console.log("in generate password now")
+    const charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let password = "";
+
+    for (let i = 0; i < length; i++)
+    {
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+    return password;
+    }
+
+function sendPasswordResetEmail(email, password)
+{
+  // function to send reset password email from our email
+  //crowdsourcedtravelplanner@gmail.com
+  //danmelissamatthew
+  console.log("in send password reset now")
+  const nodemailer = require("nodemailer");
+
+  let transporter = nodemailer.createTransport(
+  {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth:
+    {
+      user: "crowdsourcedtravelplanner@gmail.com",
+      pass: "danmelissamatthew"
+    }
+  });
+
+  let mailOptions =
+  {
+    from: "crowdsourcedtravelplanner@gmail.com",
+    to: email,
+    subject: "Password Reset",
+    text: `Your new password is: ${password}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info)
+  {
+    if (error)
+    {
+      console.log(error);
+    } else
+    {
+      console.log("Message sent: %s", info.messageId);
+    }
+  });
 }
