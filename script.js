5e5ee4ae036747d1f34ec0ab974e28fc07220956
Matthew Wong <wongmat@oregonstate.edu>
2023-03-05 11:00:09 -0800

    added in the scripts.js logic for the buttons to highlight green

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+// this makes all the nav buttons green or not green
+$(document).ready(function() {
+    //trips
+    $('.index-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // Load trips template in the right column
+    });
+    //add
+    $('.navigationAdd-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // Load add experiences template in the right column
+    });
+    //search
+    $('.navigationSearch-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // Load search experiences template in the right column
+    });
+    //save
+    $('.navigationSaved-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // Load saved experiences template in the right column
+    });
+
+    $('.navigationReset-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // Load reset password template in the right column
+    });
+
+    $('.navigationLogout-button').click(function() {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        // redirect to logout
+    });
+
+    $('#search-form').on('submit', function(event) {
+      event.preventDefault();
+    });
+});
+
 function login()
 {
   var username = document.getElementById("username").value;
